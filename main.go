f4443a5e758ea6c1a992434259d802e4e4430fe5
Emanuel Turis <hello@emanuelturis.com>
2023-12-14 12:42:18 +0000

    set up handler with initial auth routes

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"github.com/flosch/pongo2/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/webdevfuel/projectmotor/handler"
 	"github.com/webdevfuel/projectmotor/template"
 )
 
 func main() {
+	h := handler.NewHandler(handler.HandlerOptions{})
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	fs := http.FileServer(http.Dir("./dist"))
@@ -17,5 +19,6 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		template.Dashboard.ExecuteWriter(pongo2.Context{}, w)
 	})
+	r.Get("/login", h.Login)
 	http.ListenAndServe("localhost:3000", r)
 }
